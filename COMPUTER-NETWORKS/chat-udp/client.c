34ce152182771a2139a633c686b54d22bcd68b9b
Praveen <104721138+Praveenanand333@users.noreply.github.com>
2023-08-08 14:07:33 +0530

    Update client.c

--- COMPUTER-NETWORKS/chat-udp/client.c
+++ COMPUTER-NETWORKS/chat-udp/client.c
@@ -1,53 +1,50 @@
-// client.c
-#include <stdio.h>
+// Client side implementation of UDP client-server model
+#include <bits/stdc++.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 #include <arpa/inet.h>
+#include <netinet/in.h>
 
-#define PORT 8888
-#define BUFFER_SIZE 1024
+#define PORT	 8080
+#define MAXLINE 1024
 
+// Driver code
 int main() {
-    int client_fd;
-    struct sockaddr_in server_addr;
-    char buffer[BUFFER_SIZE] = {0};
+	int sockfd;
+	char buffer[MAXLINE];
+	const char *hello = "Hello from client";
+	struct sockaddr_in	 servaddr;
 
-    // Create UDP socket
-    if ((client_fd = socket(AF_INET, SOCK_DGRAM, 0)) == 0) {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
-    }
+	// Creating socket file descriptor
+	if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+		perror("socket creation failed");
+		exit(EXIT_FAILURE);
+	}
 
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(PORT);
+	memset(&servaddr, 0, sizeof(servaddr));
+	
+	// Filling server information
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(PORT);
+	servaddr.sin_addr.s_addr = INADDR_ANY;
+	
+	int n;
+	socklen_t len;
+	
+	sendto(sockfd, (const char *)hello, strlen(hello),
+		MSG_CONFIRM, (const struct sockaddr *) &servaddr,
+			sizeof(servaddr));
+	std::cout<<"Hello message sent."<<std::endl;
+		
+	n = recvfrom(sockfd, (char *)buffer, MAXLINE,
+				MSG_WAITALL, (struct sockaddr *) &servaddr,
+				&len);
+	buffer[n] = '\0';
+	std::cout<<"Server :"<<buffer<<std::endl;
 
-    // Convert IPv4 and IPv6 addresses from text to binary form
-    if (inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr) <= 0) {
-        perror("Invalid address/ Address not supported");
-        exit(EXIT_FAILURE);
-    }
-
-    while (1) {
-        // Read input from the user
-        printf("You: ");
-        fgets(buffer, BUFFER_SIZE, stdin);
-
-        // Send the message to the server
-        sendto(client_fd, buffer, strlen(buffer), 0, (struct sockaddr *)&server_addr, sizeof(server_addr));
-
-        // Receive the echo from the server
-        ssize_t recv_len = recvfrom(client_fd, buffer, BUFFER_SIZE, 0, NULL, NULL);
-        if (recv_len < 0) {
-            perror("recvfrom failed");
-            break;
-        }
-
-        // Print the received message from the server
-        printf("Server: %s", buffer);
-    }
-
-    close(client_fd);
-
-    return 0;
+	close(sockfd);
+	return 0;
 }
