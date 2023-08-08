6081b582d6fcb482400bcd91ea222aa09d59da12
Praveen <104721138+Praveenanand333@users.noreply.github.com>
2023-08-08 14:07:07 +0530

    Update server.c

--- COMPUTER-NETWORKS/chat-udp/server.c
+++ COMPUTER-NETWORKS/chat-udp/server.c
@@ -1,56 +1,59 @@
-// server.c
-#include <stdio.h>
+// Server side implementation of UDP client-server model
+#include <bits/stdc++.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 #include <arpa/inet.h>
+#include <netinet/in.h>
 
-#define PORT 8888
-#define BUFFER_SIZE 1024
+#define PORT	 8080
+#define MAXLINE 1024
 
+// Driver code
 int main() {
-    int server_fd;
-    struct sockaddr_in server_addr, client_addr;
-    socklen_t addr_len = sizeof(client_addr);
-    char buffer[BUFFER_SIZE] = {0};
+	int sockfd;
+	char buffer[MAXLINE];
+	const char *hello = "Hello from server";
+	struct sockaddr_in servaddr, cliaddr;
+	
+	// Creating socket file descriptor
+	if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+		perror("socket creation failed");
+		exit(EXIT_FAILURE);
+	}
+	
+	memset(&servaddr, 0, sizeof(servaddr));
+	memset(&cliaddr, 0, sizeof(cliaddr));
+	
+	// Filling server information
+	servaddr.sin_family = AF_INET; // IPv4
+	servaddr.sin_addr.s_addr = INADDR_ANY;
+	servaddr.sin_port = htons(PORT);
+	
+	// Bind the socket with the server address
+	if ( bind(sockfd, (const struct sockaddr *)&servaddr,
+			sizeof(servaddr)) < 0 )
+	{
+		perror("bind failed");
+		exit(EXIT_FAILURE);
+	}
+	
+	socklen_t len;
+int n;
 
-    // Create UDP socket
-    if ((server_fd = socket(AF_INET, SOCK_DGRAM, 0)) == 0) {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
-    }
+	len = sizeof(cliaddr); //len is value/result
 
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(PORT);
-
-    // Bind the socket to a specific IP and port
-    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
-    }
-
-    printf("Chat server is listening on port %d...\n", PORT);
-
-    while (1) {
-        // Receive data from the client
-        ssize_t recv_len = recvfrom(server_fd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&client_addr, &addr_len);
-        if (recv_len < 0) {
-            perror("recvfrom failed");
-            break;
-        }
-
-        // Print the received message from the client
-        printf("Received: %s", buffer);
-
-        // Send the data back to the client
-        if (sendto(server_fd, buffer, recv_len, 0, (struct sockaddr *)&client_addr, addr_len) < 0) {
-            perror("sendto failed");
-            break;
-        }
-    }
-
-    close(server_fd);
-
-    return 0;
+	n = recvfrom(sockfd, (char *)buffer, MAXLINE,
+				MSG_WAITALL, ( struct sockaddr *) &cliaddr,
+				&len);
+	buffer[n] = '\0';
+	printf("Client : %s\n", buffer);
+	sendto(sockfd, (const char *)hello, strlen(hello),
+		MSG_CONFIRM, (const struct sockaddr *) &cliaddr,
+			len);
+	std::cout<<"Hello message sent."<<std::endl;
+	
+	return 0;
 }
