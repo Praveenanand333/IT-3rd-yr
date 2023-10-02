a59fa22549ec678a7e964606552c4544fdb76742
Praveen <104721138+Praveenanand333@users.noreply.github.com>
2023-10-02 09:36:15 +0530

    Update PrimeFactor.java

--- Java/22-9/PrimeFactor.java
+++ Java/22-9/PrimeFactor.java
@@ -1,5 +1,5 @@
-import java.util.Scanner;
 import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
 
 public class PrimeFactor {
@@ -19,29 +19,18 @@ public class PrimeFactor {
     }
 
     private static boolean isPrime(int n) {
-        if (n <= 1) {
-            return false;
-        }
-        if (n <= 3) {
-            return true;
-        }
-        if (n % 2 == 0 || n % 3 == 0) {
-            return false;
-        }
+        if (n <= 1) return false;
+        if (n <= 3) return true;
+        if (n % 2 == 0 || n % 3 == 0) return false;
         for (int i = 5; i * i <= n; i += 6) {
-            if (n % i == 0 || n % (i + 2) == 0) {
-                return false;
-            }
+            if (n % i == 0 || n % (i + 2) == 0) return false;
         }
         return true;
     }
+
     private static Set<Integer> getUniquePrimeFactors(int n) {
         Set<Integer> primeFactors = new HashSet<>();
-        while (n % 2 == 0) {
-            primeFactors.add(2);
-            n /= 2;
-        }
-        for (int i = 3; i <= Math.sqrt(n); i += 2) {
+        for (int i = 2; i <= Math.sqrt(n); i++) {
             while (n % i == 0) {
                 primeFactors.add(i);
                 n /= i;
