a70d127ae407abe499343ddc0a4b974a6da3f3bd
Praveen <104721138+Praveenanand333@users.noreply.github.com>
2023-10-09 19:21:39 +0530

    Update tclient.c

--- COMPUTER-NETWORKS/socket/tcp/tclient.c
+++ COMPUTER-NETWORKS/socket/tcp/tclient.c
@@ -5,23 +5,23 @@
 #include<arpa/inet.h>
 #define BUFFER_SIZE 1024
 int main(){
-    int client_socket;
+    int server_socket;
     char rbuf[BUFFER_SIZE],sbuf[BUFFER_SIZE];
-    client_socket=socket(AF_INET,SOCK_STREAM,0);
+    server_socket=socket(AF_INET,SOCK_STREAM,0);
     struct sockaddr_in serv_addr;
     serv_addr.sin_family=AF_INET;
     serv_addr.sin_port=htons(2000);
     serv_addr.sin_addr.s_addr=inet_addr("127.0.0.1");
-    connect(client_socket,(struct sockaddr *)&serv_addr,sizeof(serv_addr));
+    connect(server_socket,(struct sockaddr *)&serv_addr,sizeof(serv_addr));
     while(1){
     printf("Enter the message:");
     scanf("%s",sbuf);
-    send(client_socket,sbuf,strlen(sbuf),0);
-    recv(client_socket,rbuf,BUFFER_SIZE,0);
+    send(server_socket,sbuf,strlen(sbuf),0);
+    recv(server_socket,rbuf,BUFFER_SIZE,0);
     printf("servers response:%s\n",rbuf);
     memset(rbuf, '\0', BUFFER_SIZE);
-    memset(sbuf, '\0', BUFFER_SIZE);
+    memset(sbuf, '\0', BUFFER_SIZE); 
     }
-    close(client_socket);
+    close(server_socket);
     return 0;
 }
