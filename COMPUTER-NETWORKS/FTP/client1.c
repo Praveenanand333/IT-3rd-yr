4e76d870013a33b64587cafab8f169b86a78283b
Praveen <104721138+Praveenanand333@users.noreply.github.com>
2023-11-20 14:24:35 +0530

    Update client1.c

--- COMPUTER-NETWORKS/FTP/client1.c
+++ COMPUTER-NETWORKS/FTP/client1.c
@@ -13,53 +13,30 @@ int main() {
     char buffer[MAX_BUFFER_SIZE];
     FILE *file;
 
-    // Create socket
     client_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (client_socket == -1) {
-        perror("Error creating socket");
-        exit(EXIT_FAILURE);
-    }
-
-    // Set up server address
     memset(&server_address, 0, sizeof(server_address));
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(PORT);
-    server_address.sin_addr.s_addr = inet_addr("127.0.0.1"); // Server IP address
+    server_address.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    // Connect to the server
-    if (connect(client_socket, (struct sockaddr*)&server_address, sizeof(server_address)) == -1) {
-        perror("Error connecting to server");
-        close(client_socket);
-        exit(EXIT_FAILURE);
-    }
+    connect(client_socket, (struct sockaddr*)&server_address, sizeof(server_address));
 
-    // Get the filename from the user
     printf("Enter the filename to request: ");
     fgets(buffer, MAX_BUFFER_SIZE, stdin);
-    buffer[strcspn(buffer, "\n")] = '\0'; // Remove the newline character
+    buffer[strcspn(buffer, "\n")] = '\0';
 
-    // Send the filename to the server
     send(client_socket, buffer, strlen(buffer), 0);
-
-    // Receive response from the server
     recv(client_socket, buffer, MAX_BUFFER_SIZE, 0);
 
     if (strcmp(buffer, "File not found") == 0) {
         printf("File not found on the server.\n");
     } else {
-        // Open a new file to save the received data
         file = fopen("received_file", "wb");
-        if (file == NULL) {
-            perror("Error opening file");
-            close(client_socket);
-            exit(EXIT_FAILURE);
-        }
 
-        // Receive and save file contents in chunks
         while (1) {
             ssize_t bytes_received = recv(client_socket, buffer, MAX_BUFFER_SIZE, 0);
             if (bytes_received <= 0) {
-                break; // End of file or error
+                break;
             }
             fwrite(buffer, 1, bytes_received, file);
         }
@@ -68,7 +45,6 @@ int main() {
         printf("File received from the server.\n");
     }
 
-    // Close the socket
     close(client_socket);
 
     return 0;
