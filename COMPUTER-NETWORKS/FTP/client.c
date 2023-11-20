be92517ea8afc113717c376fbb46333ce977fdec
Praveen <104721138+Praveenanand333@users.noreply.github.com>
2023-11-20 21:20:12 +0530

    Add files via upload

---
+++ COMPUTER-NETWORKS/FTP/client.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define SERVER_IP "127.0.0.1"
+#define MAX_FILENAME_SIZE 256
+
+void receive_file(const char *filename, int sockfd) {
+    FILE *file = fopen("received_file.txt", "wb");
+    if (file == NULL) {
+        perror("Error creating file");
+        exit(EXIT_FAILURE);
+    }
+
+    char buffer[1024];
+    int bytesRead;
+
+    while ((bytesRead = recv(sockfd, buffer, sizeof(buffer), 0)) > 0) {
+        fwrite(buffer, 1, bytesRead, file);
+    }
+
+    fclose(file);
+}
+
+int main() {
+    int sockfd;
+    struct sockaddr_in server_addr;
+
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) exit(EXIT_FAILURE);
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+
+    if (inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr) <= 0) exit(EXIT_FAILURE);
+
+    if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) exit(EXIT_FAILURE);
+
+    char filename[MAX_FILENAME_SIZE];
+    memset(filename,'\0',MAX_FILENAME_SIZE);
+    printf("Enter the filename to request: ");
+    scanf("%s",filename);
+    // fgets(filename, sizeof(filename), stdin);
+    // filename[strcspn(filename, "\n")] = '\0';  // Remove newline character
+
+    send(sockfd, filename, strlen(filename), 0);
+
+    printf("Requesting file: %s\n", filename);
+
+    receive_file(filename, sockfd);
+
+    close(sockfd);
+
+    printf("File received successfully.\n");
+
+    return 0;
+}
---
+++ COMPUTER-NETWORKS/FTP/server.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define MAX_FILENAME_SIZE 256
+
+void send_file(const char *filename, int sockfd) {
+    FILE *file = fopen(filename, "rb");
+    if (file == NULL) {
+        perror("File not found");
+        return;
+    }
+
+    char buffer[1024];
+    size_t bytesRead;
+
+    while ((bytesRead = fread(buffer, 1, sizeof(buffer), file)) > 0) {
+        if (send(sockfd, buffer, bytesRead, 0) < 0) {
+            perror("Error sending file");
+            fclose(file);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    fclose(file);
+}
+
+int main() {
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) exit(EXIT_FAILURE);
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) exit(EXIT_FAILURE);
+
+    if (listen(server_fd, 3) < 0) exit(EXIT_FAILURE);
+
+    printf("FTP Server listening on port %d...\n", PORT);
+
+    while (1) {
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) exit(EXIT_FAILURE);
+
+        char filename[MAX_FILENAME_SIZE];
+        memset(filename,'\0',MAX_FILENAME_SIZE);
+        if (recv(new_socket, filename, sizeof(filename), 0) <= 0) {
+            perror("Error receiving filename");
+            close(new_socket);
+            continue;
+        }
+        
+
+        printf("File request received: %s\n", filename);
+ char cwd[1024];
+ getcwd(cwd, sizeof(cwd));
+  printf("Current working directory: %s\n", cwd);
+        send_file(filename, new_socket);
+
+        close(new_socket);
+    }
+
+    return 0;
+}
